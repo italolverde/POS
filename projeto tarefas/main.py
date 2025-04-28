0073ac149cb7c83a12aa7adc06cd05ca8515dba9
italolverde <italolverde3@gmail.com>
2025-04-28 16:17:37 -0300

    aula pratica 2

--- projeto tarefas/main.py
+++ projeto tarefas/main.py
@@ -1,19 +1,27 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Form
 from models import Tarefa
 from typing import List
+
+#uvicorn main:app RODA O APP
 
 app = FastAPI()
 
 tarefas: List[Tarefa] = []
+id = 0
+
+#USAREMOS JQUERY E AJAX
 
 @app.get("/tarefas/",response_model=List[Tarefa])
 def listar_tarefas():
     return tarefas
 
 @app.post("/tarefas/",response_model=Tarefa)
-def criar_tarefa(tarefa:Tarefa):
+def criar_tarefa(titulo: str = Form(...), descricao: str = Form(...)):
     
-    tarefas.append(tarefa)
+    id_tarefa = id
+    id += 1
+    nova_tarefa = Tarefa(id=id_novo, titulo=titulo, descricao=descricao, concluido=False)
+    tarefas.append(nova_tarefa)
     return tarefa
 
 @app.delete("/tarefas/{tarefa_id}",response_model=Tarefa)
